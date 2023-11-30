c92e07d8578b110d94c6a35c162e45d967471f8d
wangxiangyue <>
2023-11-30 16:49:55 +0800

    初始化

---
+++ src/components/view/ViewJob.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import { ref } from 'vue'
+
+    export default {
+        setup() {
+            const count = ref(0)
+            return { count }
+        }
+    }
+</script>
+
+<template>
+    <div class=".view ">111</div>
+</template>
+
+<style>
+.view {
+    color: red;
+}
+</style>
