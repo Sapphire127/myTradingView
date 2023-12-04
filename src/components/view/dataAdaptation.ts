491ab09762dba962ac758966f8a48d80a3c896b2
wangxiangyue <>
2023-12-04 17:54:24 +0800

    添加数据转换函数

--- src/components/view/dataAdaptation.ts
+++ src/components/view/dataAdaptation.ts
@@ -1,12 +1,15 @@
-// function dataAdaptation(data: string) {
-//     const jsonObj = JSON.parse(jsonStr);
+export function dataAdaptation(data: string) {
 
-//     const result: string[] = [];
+    const jsonObj = JSON.parse(data);
+    const result: string[] = [];
 
-//     for (const key in jsonObj) {
-//         if (jsonObj.hasOwnProperty(key)) {
-//             jsonObj[key].time = key;
-//             result.push(jsonObj[key]);
-//         }
-//     }
-// }
+    for (const key in jsonObj) {
+	    if (Object.prototype.hasOwnProperty.call(jsonObj, key)) {
+		    jsonObj[key].time = key;
+		    result.push(jsonObj[key]);
+
+            console.log("res:" + JSON.stringify(result))
+	    }
+    }
+    return result;
+}
